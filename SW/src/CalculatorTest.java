109d9314c350ede5590254684e997142dfac987f
xkll1360 <xkll1360@localhost>
2026-06-02 00:22:46 +0900

    숫자변

--- SW/src/CalculatorTest.java
+++ SW/src/CalculatorTest.java
@@ -19,12 +19,12 @@ public class CalculatorTest {
     @Test
     public void testMultiplyInt() {
         Calculator calc = new Calculator();
-        assertEquals(24, calc.multiplyInt(4, 6), "4 * 6은 24여야 합니다.");
+        assertEquals(30, calc.multiplyInt(5, 6), "4 * 6은 24여야 합니다.");
     }
 
     @Test
     public void testDivideInt() {
         Calculator calc = new Calculator();
-        assertEquals(4, calc.divideInt(20, 5), "20 / 5는 4여야 합니다.");
+        assertEquals(5, calc.divideInt(20, 4), "20 / 5는 4여야 합니다.");
     }
 }
